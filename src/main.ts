3df8f6c98778a741ecb949c5dfee8e9acfb17780
Zhuang-SF <wangz.today@gmail.com>
2021-02-02 11:34:42 +0800

    add:config: 暴露本地public, 用作图床

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
+import * as serveStatic from 'serve-static';
+import { NestExpressApplication } from '@nestjs/platform-express';
+const path = require('path');
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
   app.enableCors();
+
+  console.log('path: ', __dirname);
+  app.use(
+    '/public',
+    serveStatic(path.join(__dirname, '../public'), {
+      maxAge: '1d',
+      extensions: ['jpg', 'jpeg', 'png', 'gif'],
+    }),
+  );
+
+  // app.useStaticAssets(path.join(__dirname, '../public/'))
+
   await app.listen(3000);
+  // app.useStaticAssets('/public');
 }
 bootstrap();
