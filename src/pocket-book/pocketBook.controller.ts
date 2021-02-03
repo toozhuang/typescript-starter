bbfbeee81c50a4e954c2e3dc5d645b6dbbf37181
Zhuang-SF <wangz.today@gmail.com>
2021-02-03 10:18:44 +0800

    add: 支持 AWS S3 图片上传

--- src/pocket-book/pocketBook.controller.ts
+++ src/pocket-book/pocketBook.controller.ts
@@ -11,8 +11,36 @@ import { CreatePocketbookDto } from './dto/create-pocketbook-dto';
 import { UserDecorator } from '../user/user.decorator';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { extname } from 'path';
+import { awsToken } from '../config/aws.token';
 
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
 const multer = require('multer');
+
+aws.config.update(awsToken);
+
+const s3 = new aws.S3();
+
+const uploadType = {
+  localStorage: multer.diskStorage({
+    destination: './public/cover',
+    filename: (req, file, cb) => {
+      const randomName = Array(32)
+        .fill(null)
+        .map(() => Math.round(Math.random() * 16).toString(16))
+        .join('');
+      return cb(null, `${randomName}${extname(file.originalname)}`);
+    },
+  }),
+  cloudStorage: multerS3({
+    s3: s3,
+    bucket: 'demoofergou', // bucket name
+    key: function(req, file, cb) {
+      console.log(file);
+      cb(null, file.originalname); //use Date.now() for unique file keys
+    },
+  }),
+};
 
 @Controller('pocket_book')
 export class PocketBookController {
@@ -56,21 +84,13 @@ export class PocketBookController {
   @Post('cover')
   @UseInterceptors(
     FileInterceptor('file', {
-      storage: multer.diskStorage({
-        destination: './public/cover',
-        filename: (req, file, cb) => {
-          const randomName = Array(32)
-            .fill(null)
-            .map(() => Math.round(Math.random() * 16).toString(16))
-            .join('');
-          return cb(null, `${randomName}${extname(file.originalname)}`);
-        },
-      }),
+      storage: uploadType.cloudStorage,
     }),
   )
   async addPocketBookCover(@UploadedFile() file) {
+    console.log('来了吗:::: ', file);
     // TODO: 在这里把 发过来的 file 存储到 github 上去, 然后返回 url 地址
     // Note: 目前是存储图片到当前的项目中; 更方便
-    return { path: file.path, code: 200 };
+    return { path: file.location, code: 200 };
   }
 }
--- src/pocket-record/pocket-record.controller.ts
+++ src/pocket-record/pocket-record.controller.ts
@@ -3,12 +3,10 @@ import { PocketRecordService } from './pocket-record.service';
 
 @Controller('pocket_record')
 export class PocketRecordController {
-
-  constructor(private readonly pocketRecordService: PocketRecordService) {
-  }
+  constructor(private readonly pocketRecordService: PocketRecordService) {}
 
   @Get('list')
   async listRecords() {
-    return this.pocketRecordService.listRecord()
+    return this.pocketRecordService.listRecord();
   }
 }
