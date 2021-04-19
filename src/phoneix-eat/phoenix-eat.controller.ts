355d4a0cdaa3f5d7bbe70d99bc8c214251d5f8e8
wang <wangz.today@gmail.com>
2021-04-19 11:50:42 +0800

    addd

--- src/phoneix-eat/phoenix-eat.controller.ts
+++ src/phoneix-eat/phoenix-eat.controller.ts
@@ -15,7 +15,7 @@ export class PhoenixEatController {
    *
    * @param selectedDate
    * @param week 星期， 这个是写死的， 通过这种 ？ 的形式 + ParamPathPipe 来控制传入的参数必须要
-   * 是 week 才可以
+   * 是 week 才可以 .
    * 这样写的目的是为了灵活的使用一个controller来解决业务输入的逻辑
    */
   @Get('list/:week?/:selectedDate?')
