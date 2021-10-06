729c4603b8f10a5bf0bc8dc0a63dbba893c0f3bf
toozhuang <wangz.today@gmail.com>
2021-10-06 23:39:31 +0800

    add: set for webhook only

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -5,22 +5,26 @@ import { Observable } from 'rxjs';
 export class WebhookService {
   constructor(private httpService: HttpService) {}
 
-  resendToWeChat(last_commit: any): Observable<any> {
+  resendToWeChat(last_commit: any): { message: string } {
     const wechat_webhook =
       'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c0370b72-eb11-4807-81f1-f827362001bf';
     // 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=9eb501c6-5498-4cad-8f25-7f0fa9db9455'; // node 服务端
 
-    const message = {
-      msgtype: 'markdown',
-      markdown: {
-        content: `### Merge request: \n**description**: ${last_commit.message}**url**: ${last_commit.url}`,
-      },
-    };
+    // const message = {
+    //   msgtype: 'markdown',
+    //   markdown: {
+    //     content: `### Merge request: \n**description**: ${last_commit.message}**url**: ${last_commit.url}`,
+    //   },
+    // };
+    //
+    // return this.httpService.post(wechat_webhook, JSON.stringify(message), {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    // });
 
-    return this.httpService.post(wechat_webhook, JSON.stringify(message), {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    return {
+      message: 'disable for now',
+    };
   }
 }
